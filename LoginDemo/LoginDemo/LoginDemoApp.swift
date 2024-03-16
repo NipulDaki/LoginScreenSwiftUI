f3df83c7f6dd9c54fad39a48465771876a982746
Nipul Daki <dakinipul@gmail.com>
2024-03-16 11:39:24 +0530

    - Initial commit for Repo

---
+++ LoginDemo/LoginDemo/LoginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginDemoApp.swift
+//  LoginDemo
+//
+//  Created by Nipul Daki on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
